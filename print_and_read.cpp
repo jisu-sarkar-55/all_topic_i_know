a46176b26347ad2cc6e967df59fcc5de2339dbdf
jisu-sarkar-55 <sarkar55jisu@gmail.com>
2025-09-17 15:46:39 +0530

    print_and_read file added (cin and cout)

---
+++ print_and_read.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Print "Hello, World!" to the console
+    cout << "Hello, World!" << endl;
+
+    // Wait for user input before closing
+    cin >> ws; // input will store in ws variable 
+    
+    return 0;
+}
