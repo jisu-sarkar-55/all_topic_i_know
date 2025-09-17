ee6ffc5ff7ec42100c1df26826a5e048224ef24e
jisu-sarkar-55 <sarkar55jisu@gmail.com>
2025-09-17 15:55:20 +0530

    Upadated the print_and_readline file

--- print_and_read.cpp
+++ print_and_read.cpp
@@ -8,6 +8,11 @@ int main() {
 
     // Wait for user input before closing
     cin >> ws; // input will store in ws variable 
-    
+
+    // this is comment.
+
+    // for new line use "\n" or "endl"  ->
+    cout << "Hello, World!" << "\n";
+    cout << "Hello, World!" << endl;
     return 0;
 }
